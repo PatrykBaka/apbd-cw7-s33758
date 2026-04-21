85ce832b60e83a0405c213ac9395735528f83c59
PatrykBaka <s33758@pjwstk.edu.pl>
2026-04-21 14:43:53 +0200

    stworzenie HttpPost oraz Dto potrzebnego do Posta

--- cw7/cw7/Controllers/AppointmentsController.cs
+++ cw7/cw7/Controllers/AppointmentsController.cs
@@ -1,4 +1,5 @@
-﻿using cw7.DTO;
+﻿using System.Data;
+using cw7.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 
@@ -107,7 +108,77 @@ public class AppointmentsController : ControllerBase
         
         return NotFound("Appointment not found");
     }
-    
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] CreateAppointment ca, CancellationToken ct)
+    {
+
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        await using var con = new SqlConnection(connectionString);
+
+        await using var com = new SqlCommand();
+        
+        com.Connection = con;
+        
+        await con.OpenAsync(ct);
+
+        com.CommandText = "SELECT 1 FROM Patients WHERE IdPatient = @id AND IsActive = 1";
+        com.Parameters.Clear();
+        com.Parameters.AddWithValue("@id", ca.IdPatient);
+        
+        var result = await com.ExecuteScalarAsync(ct);
+        if (result is null)
+        {
+            return BadRequest("Patient not found or is not active");
+        }
+        
+        com.CommandText = "SELECT 1 FROM Doctors WHERE IdDoctor = @id AND IsActive = 1";
+        com.Parameters.Clear();
+        com.Parameters.AddWithValue("@id", ca.IdDoctor);
+        
+        result = await com.ExecuteScalarAsync(ct);
+        if (result is null)
+        {
+            return BadRequest("Doctor not found or is not active");
+        }
+
+        if (ca.AppointmentDate < DateTime.Now)
+        {
+            return BadRequest("Appointment date has to be in the future");
+        }
+
+        if (ca.Reason.Length > 250)
+        {
+            return BadRequest("Reason is too long");
+        }
+        
+        com.CommandText = "SELECT 1 FROM Appointments WHERE IdDoctor = @id AND AppointmentDate = @date AND Status = 'Scheduled'";
+        com.Parameters.Clear();
+        com.Parameters.AddWithValue("@id", ca.IdDoctor);
+        com.Parameters.AddWithValue("@date", ca.AppointmentDate);
+
+        result = await com.ExecuteScalarAsync(ct);
+        if (result is not null)
+        {
+            return Conflict("Doctor already has appointment");
+        }
+
+        com.CommandText = @"INSERT INTO Appointments (IdPatient, IdDoctor, AppointmentDate, Status, Reason, InternalNotes)
+                            VALUES (@idPatient, @idDoctor, @appointmentDate, @status, @reason, @internalNotes);
+                            SELECT CAST(SCOPE_IDENTITY() as int);";
+        com.Parameters.Clear();
+        com.Parameters.AddWithValue("@idPatient",  ca.IdPatient);
+        com.Parameters.AddWithValue("@idDoctor",  ca.IdDoctor);
+        com.Parameters.Add("@appointmentDate", SqlDbType.DateTime2).Value = ca.AppointmentDate;
+        com.Parameters.AddWithValue("@status", ca.Status);
+        com.Parameters.AddWithValue("@reason", ca.Reason);
+        com.Parameters.AddWithValue("@internalNotes", (object?)ca.InternalNotes ?? DBNull.Value);
+
+        var newId = (int)await com.ExecuteScalarAsync(ct);
+        
+        return CreatedAtAction(nameof(GetAsyncById), new { id = newId }, ca);
+    }
     
     
 }
---
+++ cw7/cw7/DTO/CreateAppointment.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cw7.DTO;
+
+public class CreateAppointment
+{
+    
+    public int IdPatient { get; set; }
+    public int IdDoctor { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Status { get; set; }
+    [Required]
+    public string Reason { get; set; }
+
+    public string? InternalNotes { get; set; }
+    
+}
